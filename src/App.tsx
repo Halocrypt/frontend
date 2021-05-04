d198d7f350e683dfbfa1e6ca9f73852e7eb4c534
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-04 22:39:10 +0530

    vercel footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { EVENT, IS_INTRA } from "./util/constants";
 
 import { Halo2020 } from "./components/_Halo2020/Halo2020";
 import { Header } from "./components/Header/Header";
+import { Link } from "./components/ExtLink/ExtLink";
 import { Motion } from "@hydrophobefireman/ui-anim";
 import { RouteLoader } from "./components/RouteLoader";
 import { UpdateNudgeSnackbar } from "./components/UpdateNudge/UpdateNudge";
@@ -25,10 +26,22 @@ function App() {
         <Header />
         <RouteLoader />
       </Motion>
+      <VFooter />
     </main>
   );
 }
 const FE_READY = location.host.includes("localhost");
+const v = location.href.includes("vercel") || FE_READY;
+function VFooter() {
+  if (!v) return;
+  return (
+    <footer>
+      <Link href={"https://vercel.com?utm_source=halocrypt&utm_campaign=oss"}>
+        <img src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg" />
+      </Link>
+    </footer>
+  );
+}
 
 const out = !IS_INTRA && !FE_READY ? <Halo2020 /> : <App />;
 render(out, document.getElementById("app-mount"));
