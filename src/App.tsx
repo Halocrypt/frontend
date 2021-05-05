144bcef0d856f4314ef9817b859cb5fef496d6dc
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-05 18:01:12 +0530

    sticky footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,17 @@ import { EVENT, IS_INTRA } from "./util/constants";
 
 import { Header } from "./components/Header/Header";
 import { Intra } from "./components/_Messages/Intra";
-import { Link } from "./components/ExtLink/ExtLink";
 import { Motion } from "@hydrophobefireman/ui-anim";
 import { RouteLoader } from "./components/RouteLoader";
 import { UpdateNudgeSnackbar } from "./components/UpdateNudge/UpdateNudge";
-import { center } from "./style";
 import { render } from "@hydrophobefireman/ui-lib";
 import { useCredsCheck } from "./hooks/use-creds-check";
 import { useEvent } from "./hooks/use-event";
 
 function App() {
+  if (IS_INTRA) return <Intra />;
   const checked = useCredsCheck();
   const [event, error] = useEvent(EVENT);
-  if (IS_INTRA) return <Intra />;
   if (error) return <div>{error}</div>;
   if (!checked) return <div>Checking credentials..</div>;
   if (!event) return <div>Fetching event data..</div>;
@@ -27,18 +25,8 @@ function App() {
         <UpdateNudgeSnackbar />
         <Header />
         <RouteLoader />
-        <VFooter />
       </main>
     </Motion>
-  );
-}
-function VFooter() {
-  return (
-    <footer class={center}>
-      <Link href={"https://vercel.com?utm_source=halocrypt&utm_campaign=oss"}>
-        <img src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg" />
-      </Link>
-    </footer>
   );
 }
 
