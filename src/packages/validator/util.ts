6a454ad1a2c25e3e1b7f93cbf5ca89b3784a6595
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-09 15:07:21 +0530

    fix clean

--- src/packages/validator/util.ts
+++ src/packages/validator/util.ts
@@ -1,4 +1,6 @@
 export const sanitizeRegExp = /([^\w])/g;
 export const clean = (x: any) =>
-  (x + "").replace(sanitizeRegExp, "").toLowerCase();
+  String(x == null ? "" : x)
+    .replace(sanitizeRegExp, "")
+    .toLowerCase();
 export const contains = (b: any, a: any) => clean(b).includes(clean(a));
