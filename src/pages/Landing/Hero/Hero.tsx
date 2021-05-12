0509b1a2f0bca5db3d34439c9b7bb3aedbf22e85
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-12 21:07:59 +0530

    use vh instead of js

--- src/pages/Landing/Hero/Hero.tsx
+++ src/pages/Landing/Hero/Hero.tsx
@@ -1,14 +1,12 @@
 import { landingContentWrap, landingHeading, landingLogo } from "./hero.style";
 
-import { HaloIcon } from "../../../components/Icons/Halo";
+import { HaloIcon } from "@/components/Icons/Halo";
 import { IS_INTRA } from "@/util/constants";
-import { useViewportSize } from "@/hooks/use-viewport-size";
 
 export function Hero() {
-  const [height] = useViewportSize();
   return (
     <>
-      <HaloIcon className={landingLogo} height={height * 0.75} />
+      <HaloIcon className={landingLogo} height="75vh" />
       <div class={landingContentWrap}>
         <h1 class={landingHeading}>
           {IS_INTRA
