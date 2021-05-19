8d9e5087c3a655ea158f101d06a002a3d378f8f6
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-19 19:56:56 +0530

    test idb

--- src/components/UpdateNudge/UpdateNudge.tsx
+++ src/components/UpdateNudge/UpdateNudge.tsx
@@ -1,23 +1,32 @@
+import { get, set } from "@hydrophobefireman/flask-jwt-jskit";
 import { useRef, useState } from "@hydrophobefireman/ui-lib";
 
 import { Snackbar } from "../Snackbar/Snackbar";
 import { useInterval } from "@/hooks/use-interval";
 
+const key = "halo.web.version";
+function getVersion(): Promise<number> {
+  return get(key);
+}
+function setVersion(val: any) {
+  return set(key, val);
+}
 export function useLatestVersion(cancelled: boolean) {
   const [isNewVersionAvailable, setNewVersionAvailable] = useState(false);
   const currentVersion = useRef(null);
   useInterval(
     async () => {
       if (document.visibilityState === "hidden") return;
-      const version = await (
-        await fetch("/__version__.json?_vercel_no_cache=1")
-      ).json();
+      const version = await (await fetch("/__version__.json")).json();
       const { ts } = version;
       if (currentVersion.current == null) {
-        currentVersion.current = ts;
+        const prevVersion = await getVersion();
+        currentVersion.current = Math.max(prevVersion, ts);
+        if (prevVersion < ts) setVersion(ts);
         return;
       }
       if (ts > currentVersion.current) {
+        await setVersion(ts);
         setNewVersionAvailable(true);
       }
     },
