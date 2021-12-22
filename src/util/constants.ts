0d324cffd4b772864e7d310fe69f20efb86a797f
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-12-22 18:41:07 +0530

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -5,5 +5,5 @@ export const EVENT = IS_INTRA ? "intra" : "main";
 
 export const GITHUB_URL = "https://github.com/Halocrypt";
 export const INSTAGRAM_URL = "https://instagr.am/halocrypt";
-export const DISCORD_URL = "https://quic.ml/halocrypt-discord";
+export const DISCORD_URL = "https://discord.gg/npyUemcmfN";
 export const TWITTER_URL = "https://twitter.com/Halocrypt1";
